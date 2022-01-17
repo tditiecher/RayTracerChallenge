72ddcaf778c59eadfa22f2b4e5060033bf82dc82
tditiecher <taco@tdev.org>
2022-01-17 22:13:33 +0100

    CH1: Operations + static constructors

--- RayTracerChallenge.Tests/TupleTests.cs
+++ RayTracerChallenge.Tests/TupleTests.cs
@@ -3,12 +3,14 @@ using Xunit;
 
 namespace TDev.RayTracerChallenge.Tests;
 
+using static Tuple.Static;
+
 public class TupleTests
 {
     [Fact]
     public void TupleWithW1IsAPoint()
     {
-        var a = new Tuple(4.3, -4.2, 3.1, 1);
+        var a = Tuple(4.3, -4.2, 3.1, 1);
 
         a.X.Should().Be(4.3);
         a.Y.Should().Be(-4.2);
@@ -21,7 +23,7 @@ public class TupleTests
     [Fact]
     public void TupleWithW0IsAVector()
     {
-        var a = new Tuple(4.3, -4.2, 3.1, 0);
+        var a = Tuple(4.3, -4.2, 3.1, 0);
 
         a.X.Should().Be(4.3);
         a.Y.Should().Be(-4.2);
@@ -34,16 +36,146 @@ public class TupleTests
     [Fact]
     public void PointCreatesTupleWithW1()
     {
-        var p = Tuple.Point(4, -4, 3);
+        var p = Point(4, -4, 3);
 
-        p.Should().Be(new Tuple(4, -4, 3, 1));
+        p.Should().Be(Tuple(4, -4, 3, 1));
     }
 
     [Fact]
     public void VectorCreatesTupleWithW0()
     {
-        var v = Tuple.Vector(4, -4, 3);
+        var v = Vector(4, -4, 3);
 
-        v.Should().Be(new Tuple(4, -4, 3, 0));
+        v.Should().Be(Tuple(4, -4, 3, 0));
+    }
+    
+    [Fact]
+    public void AddingTwoTuples()
+    {
+        var a1 = Tuple(3, -2, 5, 1);
+        var a2 = Tuple(-2, 3, 1, 0);
+
+        a1.Add(a2).Should().Be(Tuple(1, 1, 6, 1));
+        (a1 + a2).Should().Be(Tuple(1, 1, 6, 1));
+    }
+
+    [Fact]
+    public void SubtractingTwoPoints()
+    {
+        var p1 = Point(3, 2, 1);
+        var p2 = Point(5, 6, 7);
+
+        p1.Subtract(p2).Should().Be(Vector(-2, -4, -6));
+        (p1 - p2).Should().Be(Vector(-2, -4, -6));
+    }
+
+    [Fact]
+    public void SubtractingVectorFromPoint()
+    {
+        var p = Point(3, 2, 1);
+        var v = Vector(5, 6, 7);
+
+        p.Subtract(v).Should().Be(Point(-2, -4, -6));
+        (p - v).Should().Be(Point(-2, -4, -6));
+    }
+
+    [Fact]
+    public void SubtractingTwoVectors()
+    {
+        var v1 = Vector(3, 2, 1);
+        var v2 = Vector(5, 6, 7);
+
+        v1.Subtract(v2).Should().Be(Vector(-2, -4, -6));
+        (v1 - v2).Should().Be(Vector(-2, -4, -6));
+    }
+
+    [Fact]
+    public void SubtractingVectorFromZeroVector()
+    {
+        var zero = Vector(0, 0, 0);
+        var v = Vector(1, -2, 3);
+
+        zero.Subtract(v).Should().Be(Vector(-1, 2, -3));
+        (zero - v).Should().Be(Vector(-1, 2, -3));
+    }
+
+    [Fact]
+    public void NegatingTuple()
+    {
+        var t = Tuple(1, -2, 4, -4);
+
+        (-t).Should().Be(Tuple(-1, 2, -4, 4));
+    }
+
+    [Fact]
+    public void MultiplyingTupleByScalar()
+    {
+        var a = Tuple(1, -2, 3, -4);
+
+        a.Multiply(3.5).Should().Be(Tuple(3.5, -7, 10.5, -14));
+        (a * 3.5).Should().Be(Tuple(3.5, -7, 10.5, -14));
+        (3.5 * a).Should().Be(Tuple(3.5, -7, 10.5, -14));
+    }
+
+    [Fact]
+    public void MultiplyingTupleByFraction()
+    {
+        var a = Tuple(1, -2, 3, -4);
+
+        a.Multiply(0.5).Should().Be(Tuple(0.5, -1, 1.5, -2));
+        (a * 0.5).Should().Be(Tuple(0.5, -1, 1.5, -2));
+        (0.5 * a).Should().Be(Tuple(0.5, -1, 1.5, -2));
+    }
+
+    [Fact]
+    public void DividingTupleByScalar()
+    {
+        var a = Tuple(1, -2, 3, -4);
+
+        a.Divide(2).Should().Be(Tuple(0.5, -1, 1.5, -2));
+        (a / 2).Should().Be(Tuple(0.5, -1, 1.5, -2));
+    }
+
+    [Fact]
+    public void ComputingMagnitudeOfVector()
+    { 
+        Vector(1, 0, 0).Magnitude.Should().Be(1);
+        Vector(0, 1, 0).Magnitude.Should().Be(1);
+        Vector(0, 0, 1).Magnitude.Should().Be(1);
+        Vector(1, 2, 3).Magnitude.Should().Be(Math.Sqrt(14));
+        Vector(-1, -2, -3).Magnitude.Should().Be(Math.Sqrt(14));
+    }
+
+    [Fact]
+    public void NormalizingVector()
+    {
+        Vector(4, 0, 0).Normalize().Should().Be(Vector(1, 0, 0));
+        Vector(1, 2, 3).Normalize().Should().Be(Vector(0.26726, 0.53452, 0.80178));
+    }
+
+    [Fact]
+    public void MagnitudeOfNormalizedVector()
+    {
+        Vector(1, 2, 3).Normalize().Magnitude.Should().Be(1);
+    }
+
+    [Fact]
+    public void DotProductOfTwoTuples()
+    {
+        var a = Vector(1, 2, 3);
+        var b = Vector(2, 3, 4);
+
+        a.Dot(b).Should().Be(20);
+        b.Dot(a).Should().Be(20);
+    }
+
+    [Fact]
+    public void CrossProductOfTwoVectors()
+    {
+        var a = Vector(1, 2, 3);
+        var b = Vector(2, 3, 4);
+
+        a.Cross(b).Should().Be(Vector(-1, 2, -1));
+        b.Cross(a).Should().Be(Vector(1, -2, 1));
     }
 }
--- RayTracerChallenge/Tuple.cs
+++ RayTracerChallenge/Tuple.cs
@@ -15,19 +15,46 @@ public readonly struct Tuple : IEquatable<Tuple>
         W = w;
     }
 
-    public static Tuple Point(double x, double y, double z)
-    {
-        return new Tuple(x, y, z, 1);
-    }
-
-    public static Tuple Vector(double x, double y, double z)
-    {
-        return new Tuple(x, y, z, 0);
-    }
-
     public bool IsPoint => W.EquivalentTo(1);
 
     public bool IsVector => W.EquivalentTo(0);
+
+    public double Magnitude => Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2) + Math.Pow(W, 2));
+
+    public Tuple Add(Tuple other)
+    {
+        return new Tuple(X + other.X, Y + other.Y, Z + other.Z, W + other.W);
+    }
+
+    public Tuple Subtract(Tuple other)
+    {
+        return new Tuple(X - other.X, Y - other.Y, Z - other.Z, W - other.W);
+    }
+
+    public Tuple Multiply(double value)
+    {
+        return new Tuple(X * value, Y * value, Z * value, W * value);
+    }
+
+    public Tuple Divide(double value)
+    {
+        return new Tuple(X / value, Y / value, Z / value, W / value);
+    }
+
+    public Tuple Normalize()
+    {
+        return new Tuple(X / Magnitude, Y / Magnitude, Z / Magnitude, W / Magnitude);
+    }
+
+    public double Dot(Tuple other)
+    {
+        return X * other.X + Y * other.Y + Z * other.Z + W * other.W;
+    }
+
+    public Tuple Cross(Tuple other)
+    {
+        return new Tuple(Y * other.Z - Z * other.Y, Z * other.X - X * other.Z, X * other.Y - Y * other.X, 0);
+    }
 
     public override bool Equals(object? obj)
     {
@@ -52,5 +79,54 @@ public readonly struct Tuple : IEquatable<Tuple>
     public static bool operator !=(Tuple left, Tuple right)
     {
         return !(left == right);
+    }
+
+    public static Tuple operator +(Tuple left, Tuple right)
+    {
+        return left.Add(right);
+    }
+
+    public static Tuple operator -(Tuple left, Tuple right)
+    {
+        return left.Subtract(right);
+    }
+
+    public static Tuple operator -(Tuple tuple)
+    {
+        return new Tuple(-tuple.X, -tuple.Y, -tuple.Z, -tuple.W);
+    }
+
+    public static Tuple operator *(Tuple tuple, double value)
+    {
+        return tuple.Multiply(value);
+    }
+
+    public static Tuple operator *(double value, Tuple tuple)
+    {
+        return tuple.Multiply(value);
+    }
+
+    public static Tuple operator /(Tuple tuple, double value)
+    {
+        return tuple.Divide(value);
+    }
+    
+    
+    public static class Static
+    {
+        public static Tuple Tuple(double x, double y, double z, double w)
+        {
+            return new Tuple(x, y, z, w);
+        }
+    
+        public static Tuple Point(double x, double y, double z)
+        {
+            return new Tuple(x, y, z, 1);
+        }
+
+        public static Tuple Vector(double x, double y, double z)
+        {
+            return new Tuple(x, y, z, 0);
+        }
     }
 }
