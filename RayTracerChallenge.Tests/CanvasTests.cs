f884217368f6638f2d14b5f4889c1efbd11ae835
tditiecher <taco@tdev.org>
2022-01-18 00:02:40 +0100

    CH2: Creating and saving a canvas

---
+++ RayTracerChallenge.Tests/CanvasTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TDev.RayTracerChallenge.Tests;
+
+using static Color.Static;
+
+public class CanvasTests
+{
+    [Fact]
+    public void CreatingCanvas()
+    {
+        var canvas = new Canvas(10, 20);
+
+        var black = Color(0, 0, 0);
+
+        for (var x = 0; x < canvas.Width; x++)
+        for (var y = 0; y < canvas.Height; y++)
+            canvas[x, y].Should().Be(black);
+    }
+
+    [Fact]
+    public void ReadingAndWritingPixels()
+    {
+        var canvas = new Canvas(2, 4);
+        canvas[1, 3].Should().Be(Color(0, 0, 0));
+        canvas[1, 3] = Color(0.5, 0.5, 0.5);
+        canvas[1, 3].Should().Be(Color(0.5, 0.5, 0.5));
+        canvas[1, 3] += Color(0.2, 0.2, 0.2);
+        canvas[1, 3].Should().Be(Color(0.7, 0.7, 0.7));
+    }
+
+    [Fact]
+    public async Task SaveAsPortablePixelMap()
+    {
+        var canvas = new Canvas(2, 3)
+        {
+            [0, 0] = Color(-1, -1, -1),
+            [0, 1] = Color(255, 0, 0),
+            [1, 0] = Color(0, 255, 0),
+            [1, 2] = Color(300, 300, 300)
+        };
+
+        var outputFile = Path.GetTempFileName();
+
+        await canvas.SaveAsPortablePixelMap(outputFile);
+
+        var actualLines = await File.ReadAllLinesAsync(outputFile);
+
+        var expectedLines = new[]
+        {
+            "P3",
+            "2 3",
+            "255",
+            "0 0 0",
+            "255 0 0",
+            "0 0 0",
+            "0 255 0",
+            "0 0 0",
+            "255 255 255"
+        };
+
+        actualLines.Should().Equal(expectedLines);
+
+        File.Delete(outputFile);
+    }
+}
---
+++ RayTracerChallenge/Canvas.cs
@@ -0,0 +1,52 @@
+﻿namespace TDev.RayTracerChallenge;
+
+public class Canvas
+{
+    public Canvas(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        _pixels = new Color[Width, Height];
+
+        ClearPixels();
+    }
+
+    private Color[,] _pixels { get; }
+
+    public int Width { get; }
+    public int Height { get; }
+
+    public Color this[int x, int y]
+    {
+        get => _pixels[x, y];
+        set => _pixels[x, y] = value;
+    }
+
+    public async Task SaveAsPortablePixelMap(string path)
+    {
+        const int maxColorValue = 255;
+
+        await using var writer = File.CreateText(path);
+
+        await writer.WriteLineAsync("P3");
+        await writer.WriteLineAsync($"{Width} {Height}");
+        await writer.WriteLineAsync($"{maxColorValue}");
+
+        for (var x = 0; x < Width; x++)
+        for (var y = 0; y < Height; y++)
+        {
+            var color = _pixels[x, y];
+            var r = Convert.ToInt32(Math.Max(0, Math.Min(maxColorValue, color.Red)));
+            var g = Convert.ToInt32(Math.Max(0, Math.Min(maxColorValue, color.Green)));
+            var b = Convert.ToInt32(Math.Max(0, Math.Min(maxColorValue, color.Blue)));
+            await writer.WriteLineAsync($"{r} {g} {b}");
+        }
+    }
+
+    private void ClearPixels()
+    {
+        for (var x = 0; x < Width; x++)
+        for (var y = 0; y < Height; y++)
+            _pixels[x, y] = new Color(0, 0, 0);
+    }
+}
